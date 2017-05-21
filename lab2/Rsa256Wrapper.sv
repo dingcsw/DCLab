cd3544633e272835052a9c45489262d539912007
楊仲萱 <sandy@ip77-156.vpnip.cc.ntu.edu.tw>
2017-05-21 17:37:20 +0800

    new lab2

--- lab2/Rsa256Wrapper.sv
+++ lab2/Rsa256Wrapper.sv
@@ -18,14 +18,16 @@ module Rsa256Wrapper(
 	localparam S_GET_DATA = 1;
 	localparam S_WAIT_CALCULATE = 2;
 	localparam S_SEND_DATA = 3;
+    localparam S_GET_WRITE = 4;
+	localparam S_GET_ENC = 5;
 
 	logic [255:0] n_r, n_w, e_r, e_w, enc_r, enc_w, dec_r, dec_w;
-	logic [1:0] state_r, state_w;
+	logic [2:0] state_r, state_w;
 	logic [6:0] bytes_counter_r, bytes_counter_w;
 	logic [4:0] avm_address_r, avm_address_w;
 	logic avm_read_r, avm_read_w, avm_write_r, avm_write_w;
 
-	logic rsa_start_r, rsa_start_w;
+	logic rsa_start_r, rsa_start_w, read_ne_w,read_ne_r;
 	logic rsa_finished;
 	logic [255:0] rsa_dec;
 
@@ -64,8 +66,104 @@ module Rsa256Wrapper(
 
 	always_comb begin
 		// TODO
-	end
+	    n_w = n_r;
+        e_w = e_r;
+        enc_w = enc_r;
+        dec_w = dec_r;
+        avm_address_w = avm_address_r;
+        avm_read_w = avm_read_r;
+        avm_write_w = avm_write_r;
+        state_w = state_r;
+        bytes_counter_w = bytes_counter_r;
+        rsa_start_w = rsa_start_r;
+		read_ne_w = read_ne_r;
 
+        case (state_r)
+            S_GET_KEY: begin
+                StartRead(STATUS_BASE);
+                if(avm_waitrequest == 1'b0 && avm_read_r == 1'b1) begin
+                    if(avm_readdata[RX_OK_BIT] == 1'b1) begin
+                        StartRead(RX_BASE);
+						if(read_ne_r == 1'b1)
+							state_w = S_GET_DATA;
+						else
+                        	state_w = S_GET_ENC;
+                    end
+                end
+            end
+
+            S_GET_WRITE: begin
+                StartRead(STATUS_BASE);
+                if(avm_waitrequest == 1'b0 && avm_read_r == 1'b1) begin
+                    if(avm_readdata[TX_OK_BIT] == 1'b1) begin
+                        StartWrite(TX_BASE);
+                        state_w = S_SEND_DATA;
+                    end
+                end
+            end
+
+			S_GET_ENC: begin
+                if(avm_waitrequest == 1'b0 && avm_read_r == 1'b1) begin
+                    state_w = S_GET_KEY;
+                    avm_read_w = 0;
+					bytes_counter_w = bytes_counter_r + 1;
+					if(bytes_counter_r < 32 ) begin
+                        enc_w = enc_r << 8;
+                        enc_w[7:0] = avm_readdata[7:0];
+					end
+                    if(bytes_counter_r == 31) begin
+                    	rsa_start_w = 1;
+                        state_w = S_WAIT_CALCULATE;
+                        bytes_counter_w = 0;
+					end
+				end
+			end
+
+            S_GET_DATA: begin
+                if(avm_waitrequest == 1'b0 && avm_read_r == 1'b1) begin
+                    state_w = S_GET_KEY;
+                    avm_read_w = 0;
+					bytes_counter_w = bytes_counter_r + 1;
+
+                    if(bytes_counter_r < 32) begin
+                        n_w = n_r << 8;
+                        n_w[7:0] = avm_readdata[7:0];
+                    end else if(bytes_counter_r < 64) begin
+                        e_w = e_r << 8;
+                        e_w[7:0] = avm_readdata[7:0];
+					end
+					if(bytes_counter_r == 63) begin
+						read_ne_w = 1'b0;
+						bytes_counter_w = 0;
+					end
+            	end
+			end
+
+            S_WAIT_CALCULATE: begin
+                rsa_start_w = 0;
+                if(rsa_finished) begin
+                    state_w = S_GET_WRITE;
+                    dec_w = rsa_dec;
+                end
+            end
+
+            S_SEND_DATA: begin
+                if(avm_waitrequest == 1'b0 && avm_write_r == 1'b1) begin
+					avm_write_w = 0;
+					avm_read_w = 0;
+					if(bytes_counter_r < 30) begin
+						dec_w = dec_r << 8;
+						state_w = S_GET_WRITE;
+						bytes_counter_w = bytes_counter_r + 1;
+                    end else if(bytes_counter_r ==30) begin
+                        state_w = S_GET_KEY;
+                        bytes_counter_w = 0;
+						enc_w = 0;
+                    end
+				end
+            end
+        endcase
+    end
 
 	always_ff @(posedge avm_clk or posedge avm_rst) begin
 		if (avm_rst) begin
@@ -77,8 +175,9 @@ module Rsa256Wrapper(
 			avm_read_r <= 1;
 			avm_write_r <= 0;
 			state_r <= S_GET_KEY;
-			bytes_counter_r <= 63;
+			bytes_counter_r <= 0;
 			rsa_start_r <= 0;
+			read_ne_r <= 1'b1;
 		end else begin
 			n_r <= n_w;
 			e_r <= e_w;
@@ -89,7 +188,8 @@ module Rsa256Wrapper(
 			avm_write_r <= avm_write_w;
 			state_r <= state_w;
 			bytes_counter_r <= bytes_counter_w;
-			rsa_start_r <= rsa_start_w;
+            rsa_start_r <= rsa_start_w;
+			read_ne_r <= read_ne_w;
 		end
 	end
 endmodule
